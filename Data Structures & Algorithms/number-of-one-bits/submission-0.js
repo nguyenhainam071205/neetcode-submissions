dfef9698beab014f61e67a3d6a12fa4839621b1a
Nguyễn Hải Nam <namnguyen071205@gmail.com>
2026-05-07 00:38:49 +0700

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        let count = 0;
+        while(n) {
+            count += (n & 1);
+            n >>= 1;
+        }
+        return count
+    }
+}
