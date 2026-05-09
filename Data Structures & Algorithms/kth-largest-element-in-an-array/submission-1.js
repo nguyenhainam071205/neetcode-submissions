d61523563a1050dbe66af9290a81b89041569147
Nguyễn Hải Nam <namnguyen071205@gmail.com>
2026-05-09 22:08:44 +0700

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.js
@@ -0,0 +1,13 @@
+
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+        nums.sort((a, b) => b - a);
+         return nums[k - 1];
+    }
+}
